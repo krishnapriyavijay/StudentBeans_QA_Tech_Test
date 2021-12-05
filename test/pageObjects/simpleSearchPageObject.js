bbcee6592e29c6780db7d731ce0d91ebf2ab1a92
krishnapriyavijay <krishnapriyavijay199@gmail.com>
2021-12-05 23:24:20 +0000

    Development1

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -1,6 +1,9 @@
 const ParentPageObject = require('./parentPageObject')
+const {By, Key, Builder} = require('selenium-webdriver')
+const assert = require('assert')
 
 class simpleSearchPageObject extends ParentPageObject {
+
   goToHomePage () {
     // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
     browser.url('')
@@ -9,6 +12,27 @@ class simpleSearchPageObject extends ParentPageObject {
   verifyHomePage () {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
   }
+
+  goToSearchpage (){
+    const element = driver.findElement(By.name("query"))
+    driver.actions({ bridge: true }).moveToElement(element).clickAndHold().perform()
+  }
+
+  openSearchPage(){
+    const element =  driver.findElement(By.css(".\\_1bazady > div")) //path
+     driver.actions({ bridge: true }).moveToElement(element).release().perform()
+  }
+
+  enterSearchText(SearchStr ){
+    driver.findElement(By.xpath("//div[@id=\'home_root\']/div/nav/div")).click()
+    driver.findElement(By.xpath("//input[@value=\'Samsung\']")).sendKeys(SearchStr )
+  }
+
+  verifySearchResult (SearchStr){
+    assert( driver.findElement(By.xpath("//h3[contains(.,\'Discounts\')]")).isSelected())
+    assert( driver.findElement(By.xpath("//span[contains(.,\'Samsung\')]")).isSelected())
+  }
+
 }
 
 module.exports = simpleSearchPageObject
--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,9 +1,28 @@
-const { Given } = require('cucumber')
+const { Given, When, Then } = require('cucumber')
+const assert = require('assert')
+const {Builder, By, Key} = require('selenium-webdriver')
 const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
 
-const simpleSearchPageObject = new SimpleSearchPageObject()
+const simpleSearchPageObject = new SimpleSearchPageObject()    
 
-Given('I am on the studentbeans homepage', function () {
-  simpleSearchPageObject.goToHomePage()
-  simpleSearchPageObject.verifyHomePage()
+Given('I am on the studentbeans homepage', {timeout:10000}, async function () {
+  driver = await new Builder().forBrowser('chrome').build()
+  await simpleSearchPageObject.goToHomePage()
+  await driver.manage().window().setRect({ width: 1338, height: 839 })
+  await simpleSearchPageObject.verifyHomePage()
+})
+
+Given('I open the search bar', {timeout:10000},  async function () {
+  await simpleSearchPageObject.goToSearchpage()
+  await simpleSearchPageObject.openSearchPage()
+})
+
+When('I enter "Samsung"', {timeout:10000},  async function () {
+  let SearchStr = "Samsung"
+  await simpleSearchPageObject.enterSearchText(SearchStr )
+})
+
+Then('I should be shown a search listing for "Samsung"', {timeout:10000},  async function () {
+  let SearchStr = "Samsung"
+  await simpleSearchPageObject.verifySearchResult(SearchStr)
 })
